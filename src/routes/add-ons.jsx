e4a8be98dc4a8e0fec219a647aeacc5e3f4e9e04
neda-pro <neda.pro@outlook.com>
2023-12-11 19:07:07 +0100

    added  Footer component inside add-ons

--- src/routes/add-ons.jsx
+++ src/routes/add-ons.jsx
@@ -1,11 +1,18 @@
 import { useEffect } from "react";
 import { setDescription, setTitle } from "../features/navigation/headerSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setStep } from "../features/navigation/sideBarSlice";
 import { setButtons, setNav } from "../features/navigation/footerSlice";
+import { Box } from "@mui/material";
+import FooterButtons from "../components/FooterButtons";
+import { useNavigate } from "react-router-dom";
 
 const AddOns = () => {
+  const { hideBack, hideNext, isConfirm, next, previous } = useSelector(
+    (store) => store.footer
+  );
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   useEffect(() => {
     dispatch(setTitle("Pick add-ons"));
     dispatch(setDescription("Add-ons help enhance your gaming experience."));
@@ -25,7 +32,20 @@ const AddOns = () => {
     );
   }, []);
 
-  return;
+  return (
+    <Box sx={{ flex: 1, display: "flex", flexDirection: "column" }}>
+      <Box sx={{ flex: 1 }}></Box>
+      <FooterButtons
+        hideBack={hideBack}
+        hideNext={hideNext}
+        isConfirm={isConfirm}
+        onNextClick={() => {
+          navigate(`/${next}`);
+        }}
+        onPrevClick={() => navigate(`/${previous}`)}
+      />
+    </Box>
+  );
 };
 
 export default AddOns;
