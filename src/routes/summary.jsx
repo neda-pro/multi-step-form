d1ded12ebdbd138d2367f768e91b9d5298fd00b2
neda-pro <neda.pro@outlook.com>
2023-12-11 20:24:36 +0100

    FooterButton added in summary

--- src/routes/summary.jsx
+++ src/routes/summary.jsx
@@ -1,11 +1,18 @@
 import { useEffect } from "react";
 import { setDescription, setTitle } from "../features/navigation/headerSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setStep } from "../features/navigation/sideBarSlice";
 import { setButtons, setNav } from "../features/navigation/footerSlice";
+import FooterButtons from "../components/FooterButtons";
+import { Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const Summary = () => {
+  const { hideBack, hideNext, isConfirm, next, previous } = useSelector(
+    (store) => store.footer
+  );
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   useEffect(() => {
     dispatch(setTitle("Finishing up"));
     dispatch(
@@ -26,7 +33,30 @@ const Summary = () => {
       })
     );
   }, []);
-  return;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flex: 1,
+        flexDirection: "column",
+      }}
+    >
+      <Box
+        sx={{
+          flex: 1,
+        }}
+      ></Box>
+      <FooterButtons
+        hideBack={hideBack}
+        hideNext={hideNext}
+        isConfirm={isConfirm}
+        onNextClick={() => {
+          navigate(`/${next}`);
+        }}
+        onPrevClick={() => navigate(`/${previous}`)}
+      />
+    </Box>
+  );
 };
 
 export default Summary;
